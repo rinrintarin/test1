df01bbdaf5b8225b3f0ba7c81bdd9020328ce364
rin <103346269+rinrintarin@users.noreply.github.com>
2023-06-26 10:43:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,33 @@
-// Calculate total cost dynamically
-function calculateTotalCost() {
-  var subjectCheckboxes = document.querySelectorAll('input[name="subjects"]:checked');
-  var timeSlot = document.querySelector('select[name="time-slot"]').value;
-  
-  var totalCost = 0;
-  
-  // Assign prices to subjects and time slots
-  subjectCheckboxes.forEach(function(checkbox) {
-    if (checkbox.value === "Math") {
-      totalCost += 50; // Assign the price for Math
-    } else if (checkbox.value === "Science") {
-      totalCost += 60; // Assign the price for Science
+  <script>
+    function sendReceiptEmail() {
+      // Fetch the form values
+      const studentName = document.getElementById('studentName').value;
+      const email = document.getElementById('email').value;
+      const tutoringLevelSelect = document.getElementById('tutoringLevel');
+      const selectedTutoringLevel = tutoringLevelSelect.options[tutoringLevelSelect.selectedIndex];
+      const tutoringLevelPrice = selectedTutoringLevel.dataset.price;
+      const subjectSelect = document.getElementById('subject');
+      const selectedSubject = subjectSelect.options[subjectSelect.selectedIndex];
+      const subjectPrice = selectedSubject.dataset.price;
+      const totalPrice = parseInt(tutoringLevelPrice) + parseInt(subjectPrice);
+
+      // Construct the email template parameters
+      const templateParams = {
+        studentName,
+        email,
+        tutoringLevel: selectedTutoringLevel.value,
+        subject: selectedSubject.value,
+        totalPrice
+      };
+
+      // Send the email using EmailJS
+      emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', templateParams, 'YOUR_USER_ID')
+        .then(function(response) {
+          console.log('Email sent successfully!', response.status, response.text);
+          alert('Receipt email sent successfully!');
+        }, function(error) {
+          console.error('Error sending email:', error);
+          alert('Failed to send receipt email.');
+        });
     }
-    // Add more conditions for other subjects and prices
-  });
-  
-  // Update total cost on the page
-  document.getElementById('total-cost').textContent = '$' + totalCost;
-}
-
-// Listen for form submission
-document.getElementById('tutoring-form').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent form submission
-  
-  // Perform necessary actions, such as storing the information or generating a confirmation
-  
-  // Reset the form
-  this.reset();
-  
-  // Update total cost to $0
-  document.getElementById('total-cost').textContent = '$0';
-});
-
-// Listen for changes in subject checkboxes and time slot selection
-var subjectCheckboxes = document.querySelectorAll('input[name="subjects"]');
-subjectCheckboxes.forEach(function(checkbox) {
-  checkbox.addEventListener('change', calculateTotalCost);
-});
-
-var timeSlotSelect = document.querySelector('select[name="time-slot"]');
-timeSlotSelect.addEventListener('change', calculateTotalCost);
+  </script>
